4e04fad93d9e08edfc49cdf36cf9c64db1093bde
Raimundo Norberto <raimundonorberto@gmail.com>
2018-08-26 19:39:47 -0300

    Commit inicial

---
+++ dup/dup.go
@@ -0,0 +1,24 @@
+// Dup exibe o texto e toda linha que aparece mais de uma vez
+// na entrada-padrão, precedida por sua contagem
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
